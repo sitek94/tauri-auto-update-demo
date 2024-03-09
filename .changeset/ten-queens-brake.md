7ac4741f48c568ac6995b71c1017058783d7ad1b
Maciek Sitkowski <msitkowski94@gmail.com>
2024-03-09 03:23:37 +0100

    fix build

---
+++ .changeset/ten-queens-brake.md
@@ -0,0 +1,5 @@
+---
+'tauri-auto-update-demo': patch
+---
+
+fix build
--- scripts/build.js
+++ scripts/build.js
@@ -6,4 +6,6 @@ const base = 'src-tauri/target/aarch64-apple-darwin/release/bundle'
 
 Bun.write(`${base}/dmg/${name}_${version}_aarch64.dmg`, '__test__')
 Bun.write(`${base}/macos/${name}/MacOS/${name}`, '__test__')
-Bun.write(`${base}/macos/${name}.app`, '__test__')
+Bun.write(`${base}/macos/${name}.app.tar.gz`, '__test__')
+Bun.write(`${base}/macos/${name}.app/Contents/Info.plist`, '__test__')
+Bun.write(`${base}/macos/${name}.app/Contents/Resources/app.icns`, '__test__')
