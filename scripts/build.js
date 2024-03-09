fc69bcda0a0a1fa7fcd74539bf63fae7fd2d7190
Maciek Sitkowski <msitkowski94@gmail.com>
2024-03-09 03:14:08 +0100

    fix target

--- scripts/build.js
+++ scripts/build.js
@@ -2,8 +2,8 @@ import packageJson from '../package.json'
 
 const name = 'tauri-auto-update-demo'
 const version = packageJson.version
-const base = 'src-tauri/target/artifacts-aarch64/release/bundle'
+const base = 'src-tauri/target/aarch64-apple-darwin/release/bundle'
 
-Bun.write(`${base}/dmg/${name}_0.0.1_aarch64.dmg`, '__test__')
+Bun.write(`${base}/dmg/${name}_${version}_aarch64.dmg`, '__test__')
 Bun.write(`${base}/macos/${name}/MacOS/${name}`, '__test__')
 Bun.write(`${base}/macos/${name}.app`, '__test__')
